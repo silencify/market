d922c3e48259b18f4759fc325219c8384cedac0c
Rojen Maharjan <rjn.mrjn@gmail.com>
2022-06-11 19:07:52 +0545

    Install cors to allow cors, Install nodemon for hot reload #4

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = 9000;
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = 9000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello world')
